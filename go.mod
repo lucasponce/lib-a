9fc392d83b066e2f056143f76fcf4e37a1a435f5
Lucas Ponce <lponce@redhat.com>
2025-05-24 17:17:28 +0200

    v0.0.1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module lib-a
+module github.com/lucasponce/lib-a
 
 go 1.22.6
+
+require github.com/lucasponce/shared-x v0.0.1
---
+++ pkg/liba/records.go
@@ -0,0 +1,25 @@
+package liba
+
+import (
+	"fmt"
+	"github.com/lucasponce/shared-x/pkg/tracks"
+)
+
+var VERSION = "v0.0.1"
+
+func GetVersion() string {
+	return VERSION
+}
+
+func AddRecord(id string, content string) {
+	tracks.AddTrack(id, fmt.Sprintf("lib-a %s: %s", VERSION, content))
+}
+
+func DeleteRecord(id string) {
+	tracks.RemoveTrack(id)
+}
+
+func GetRecords() map[string]string {
+	tracks.DumpTracks()
+	return tracks.GetTracks()
+}
